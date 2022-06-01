2c2b769027092bfdd8664248fe92fd921b62eb9f
Muskan Malhotra <muskan01malhotra@gmail.com>
2022-06-01 16:21:08 +0530

    Climb Stairs Memoization

--- 02 ClimbStairs.java
+++ 02 ClimbStairs.java
@@ -1,5 +1,9 @@
 import java.io.*;
-
+/**
+ * Same as print maze path
+ * Time complexity: O(n)
+ * Space: O(n)
+ */
 
 class ClimbStairs{
   public static void main(String[] args) throws IOException{
@@ -7,26 +11,57 @@ class ClimbStairs{
     int n = Integer.parseInt(br.readLine());
 
     int[] qb = new int[n+1];
-    // int ans = fiboMem(n, qb);
-    CStairs(n,0,"");
-    // System.out.println(ans);
+    int ans = CStairsMem(n, qb);
+    // int ans = CStairs(n);
+    System.out.println(ans);
   }
 
   // static void count = 0;
   //here, n is the destination
-  public static void CStairs(int n, int src,String psf ){
-    if(src==n){
-      System.out.println(psf);
-      return;
+  public static int CStairs(int n){
+
+    if(n==0){
+      return 1;
     }
+    if(n<0){
+      return 0;
+    }
+    
+    System.out.println("Hello: "+ n);
+    int nm1 = CStairs(n-1);
+    int nm2 = CStairs(n-2);
+    int nm3 = CStairs(n-3);
+
+    int ans = nm1+nm2+nm3;
+
+    return ans;
+
+
+  }
+  public static int CStairsMem(int n,int[] qb){
+
+    if(n==0){
+      return 1;
+    }
+    if(n<0){
+      return 0;
+    }
+    
     
 
-    for(int jumps = 1; jumps<=n; jumps++){
-      CStairs(n,src+jumps,psf+"1");
-      CStairs(n,src+jumps+1,psf+"2");
-      CStairs(n,src+jumps+2,psf+"3");
+    if(qb[n] != 0){
+      return qb[n];
     }
-    
+    System.out.println("Hello: "+ n);
+    int nm1 = CStairsMem(n-1,qb);
+    int nm2 = CStairsMem(n-2,qb);
+    int nm3 = CStairsMem(n-3,qb);
+
+    int ans = nm1+nm2+nm3;
+
+    qb[n] =  ans;
+
+    return ans;
 
 
   }
