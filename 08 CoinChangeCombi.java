51ccc56f783acf2e3e8fcb8b9289871763d43cf7
Muskan Malhotra <muskan01malhotra@gmail.com>
2022-06-22 14:19:31 +0530

    Coin Change Combination

---
+++ 08 CoinChangeCombi.java
@@ -0,0 +1,129 @@
+
+import java.util.*;
+
+/**
+ * BEST EXAMPLE TO UNDERSTAND ON THE WAY UP RECUSRION WITH RECURSIVE COUNT
+ */
+
+class CoinChangeCombi {
+
+    // public static void main(String[] args) throws Exception {
+        
+    //     Scanner scn = new Scanner(System.in);
+    //     int n = scn.nextInt();
+        
+    //     int[] ar = new int[n];
+        
+    //     for(int i=0;i<n;i++){
+    //         ar[i] = scn.nextInt();
+    //     }
+        
+    //     int tar = scn.nextInt();
+        
+    //     TarSum(ar,tar,0,0,"");
+    //     // System.out.println(count);
+    //     scn.close();
+    // }
+    
+    // public static void TarSum(int[] ar,int tar,int j,int count,String psf){
+        
+       
+    //     if(j==ar.length-1)
+    //     {   System.out.println(count);
+    //         return;
+    //     }
+    //     if(tar == 0){
+    //         // count++;
+    //         System.out.println(tar+" "+j+" "+count+" "+psf);
+    //         return;
+    //     }
+            
+    //     // }
+        
+    //     if(tar<0){
+    //         System.out.println(tar+" "+j+" "+count+" "+psf);
+    //         return;
+    //     }
+        
+    //     for(int i=j;i<ar.length;i++){
+    //         System.out.println(tar+" "+j+" "+count+" "+psf);
+    //         TarSum(ar,tar-ar[i],j,count++,psf+ar[i]);
+    //         System.out.println("post"+tar+" "+j+" "+count+" "+psf);
+            
+    //     }
+        
+    //     j = j+1;
+        
+    //     return; 
+        
+    // }
+
+    
+    // :::::::::::::::::::::::::::::CORRECTED VERISION:::::::::::::::::::::::::::
+
+    public static void main(String[] args) throws Exception {
+        
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        
+        int[] ar = new int[n];
+        
+        for(int i=0;i<n;i++){
+            ar[i] = scn.nextInt();
+        }
+        
+        int tar = scn.nextInt();
+        
+       int count =  TarSum(ar,tar,0,0,"");
+        System.out.println(count);
+        scn.close();
+
+    }
+    
+    public static int TarSum(int[] ar,int tar,int j,int count,String psf){
+        
+        int ans = 0;
+        if(j==ar.length)
+        {   
+        // System.out.println(count);
+            return 0;
+        }
+        if(tar == 0){
+            ans++;
+            // System.out.println(tar+" "+j+" "+count+" "+psf);
+            // System.out.println(ans);
+            return ans;
+        }
+            
+        // }
+        
+        if(tar<0){
+            // System.out.println(tar+" "+j+" "+count+" "+psf);
+            return 0;
+        }
+        
+        for(int i=j;i<ar.length;i++){
+            // System.out.println(tar+" "+j+" "+count+" "+psf);
+            ans += TarSum(ar,tar-ar[i],j,count++,psf+ar[i]);
+            // System.out.println("post"+tar+" "+j+" "+count+" "+psf);
+            j = j+1;
+        }
+        
+        
+        
+        return ans;
+        
+        /**
+         * Input 4 2 3 5 6 tar=10
+         * 22222
+            2233
+            226
+            235
+            55
+            5
+         */
+        
+        
+    }
+
+}
