8c4d79018c583fa1acd2b4ba5eb0bb2b3c321942
Muskan Malhotra <muskan01malhotra@gmail.com>
2022-07-28 22:40:26 +0530

    Maximum Sum Subarray

---
+++ 11 MaxSumSubarray.java
@@ -0,0 +1,25 @@
+
+
+class MaxSumSubarray {
+	
+	public static long maxSubarraySum(int[] nums, int n) {
+		// write your code here
+        long curSum = 0;
+        long maxSum = Integer.MIN_VALUE;
+        
+        for(int i=0;i<nums.length;i++){
+            curSum = curSum+nums[i];
+            
+            
+//             System.out.println(maxSum);
+            if(curSum<0){
+                curSum = 0;
+            }
+            maxSum = Math.max(curSum,maxSum);
+        }
+        
+        return maxSum;
+    
+	}
+
+}
