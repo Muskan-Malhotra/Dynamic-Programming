157d4191b41688336ae78b8d2f051bd4de622882
Muskan Malhotra <muskan01malhotra@gmail.com>
2022-07-30 13:11:13 +0530

    Folders for Pep and AV

--- leet.java
+++ leet.java
@@ -1,114 +1,114 @@
-class Solution {
+// class Solution {
     
-  public static String compareStr(int j, String a, String[] words){
+//   public static String compareStr(int j, String a, String[] words){
       
-      String ans = "";
+//       String ans = "";
       
-      // String str = "JavaisLove";
-      // System.out.println(str.contains("is"));  //true
+//       // String str = "JavaisLove";
+//       // System.out.println(str.contains("is"));  //true
       
       
-      for(int i=j+1;i<words.length;i++){
+//       for(int i=j+1;i<words.length;i++){
           
-          // if(a.length<words.length-1){
-              // System.out.println(words[i].length()-words[i+1].length() == words[i].lastIndexOf(words[i+1]));
-             // if(a.length()<words[i].length()){
-             //     // if(words[i+1].contains(words[i])){
-             //     //     ans+=words[i+1]+"#";
-             //     // }
-             //     continue;
-             // }
-             if(words[i].contains(words[i+1]) && words[i].length()-words[i+1].length() == words[i].lastIndexOf(words[i+1])){
-                  ans+=words[i]+"#";
-                  i++;
-             } 
-          else{
-              ans+=words[i]+"#";
-           }
-          // }
+//           // if(a.length<words.length-1){
+//               // System.out.println(words[i].length()-words[i+1].length() == words[i].lastIndexOf(words[i+1]));
+//              // if(a.length()<words[i].length()){
+//              //     // if(words[i+1].contains(words[i])){
+//              //     //     ans+=words[i+1]+"#";
+//              //     // }
+//              //     continue;
+//              // }
+//              if(words[i].contains(words[i+1]) && words[i].length()-words[i+1].length() == words[i].lastIndexOf(words[i+1])){
+//                   ans+=words[i]+"#";
+//                   i++;
+//              } 
+//           else{
+//               ans+=words[i]+"#";
+//            }
+//           // }
           
-          else{
-              ans+=words[i]+"#";
-          }
-      }
+//           else{
+//               ans+=words[i]+"#";
+//           }
+//       }
       
-      return ans;
+//       return ans;
       
-  }
-  public int minimumLengthEncoding(String[] words) {
-      Arrays.sort(words, Comparator.comparingInt(String::length));
+//   }
+//   public int minimumLengthEncoding(String[] words) {
+//       Arrays.sort(words, Comparator.comparingInt(String::length));
       
-      String res = "";
+//       String res = "";
       
-      for(int i=0;i<words.length-1;i++){
+//       for(int i=0;i<words.length-1;i++){
           
-          res = compareStr(i,words[i],words);
-      }
+//           res = compareStr(i,words[i],words);
+//       }
       
-      System.out.println(res);
-      return res.length();
+//       System.out.println(res);
+//       return res.length();
       
-  }
-}
+//   }
+// }
 
 
-// ["time","me","bell"]
-// ["t"]
-// ["feipyxx","e"]
-// ["p","grah","qwosp"]
-// ["ctxbhc","c"]
+// // ["time","me","bell"]
+// // ["t"]
+// // ["feipyxx","e"]
+// // ["p","grah","qwosp"]
+// // ["ctxbhc","c"]
 
 
-class Solution {
+// class Solution {
     
-  public static String compareStr(int j, String a, String[] words,String res){
+//   public static String compareStr(int j, String a, String[] words,String res){
       
-      String ans = "";
+//       String ans = "";
       
-      // String str = "JavaisLove";
-      // System.out.println(str.contains("is"));  //true
+//       // String str = "JavaisLove";
+//       // System.out.println(str.contains("is"));  //true
       
-      if(words.length == 1){
-          return words[j]+" ";
-      }
-      for(int i=j+1;i<words.length;i++){
+//       if(words.length == 1){
+//           return words[j]+" ";
+//       }
+//       for(int i=j+1;i<words.length;i++){
           
-          if(words[i].contains(a) && words[i].length()-a.length() == words[i].lastIndexOf(a)){
-                  ans+=words[i]+"#";
-                  // i++;
-             } 
-          else if(!res.contains(words[i])){
-              ans+=words[i]+"#";
-           }
-          else if(res.contains(words[i]) && words[i].length()-a.length() == words[i].lastIndexOf(a)){
-              ans+=words[i]+"#";
-          }
+//           if(words[i].contains(a) && words[i].length()-a.length() == words[i].lastIndexOf(a)){
+//                   ans+=words[i]+"#";
+//                   // i++;
+//              } 
+//           else if(!res.contains(words[i])){
+//               ans+=words[i]+"#";
+//            }
+//           else if(res.contains(words[i]) && words[i].length()-a.length() == words[i].lastIndexOf(a)){
+//               ans+=words[i]+"#";
+//           }
       
-      }
+//       }
       
-      return ans;
+//       return ans;
       
-  }
-  public int minimumLengthEncoding(String[] words) {
-      Arrays.sort(words, Comparator.comparingInt(String::length));
+//   }
+//   public int minimumLengthEncoding(String[] words) {
+//       Arrays.sort(words, Comparator.comparingInt(String::length));
       
-      String res = "";
+//       String res = "";
       
-      for(int i=0;i<words.length-1;i++){
+//       for(int i=0;i<words.length-1;i++){
           
-          res += compareStr(i,words[i],words,res);
-          System.out.println(res);
-      }
+//           res += compareStr(i,words[i],words,res);
+//           System.out.println(res);
+//       }
       
-      // System.out.println(res);
-      return res.length();
+//       // System.out.println(res);
+//       return res.length();
       
-  }
-}
+//   }
+// }
 
 
-// ["time","me","bell"]
-// ["t"]
-// ["feipyxx","e"]
-// ["p","grah","qwosp"]
-// ["ctxbhc","c"]
+// // ["time","me","bell"]
+// // ["t"]
+// // ["feipyxx","e"]
+// // ["p","grah","qwosp"]
+// // ["ctxbhc","c"]
